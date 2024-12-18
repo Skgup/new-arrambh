2e90cb3f91c4bd400e8384868dc1ab4c77a52ff0
Skgup <99518596+Skgup@users.noreply.github.com>
2024-12-18 19:56:11 +0530

    ok

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -89,12 +89,12 @@ const HeroSection = () => {
             </div>
             <div className="relative bottom-20 lg:bottom-56  z-10 flex flex-row space-x-4  md:left-10  px-6 lg:px-[9%] md:[8%]">
         <button
-          className="bg-white text-sm text-[#85132c]  hover:text-white px-1 py-3 rounded-md shadow-md font-bold hover:bg-[#85132c] transition duration-300 w-full md:w-auto"
+          className="bg-white text-sm text-[#85132c]  hover:text-white px-4 py-3 rounded-md shadow-md font-bold hover:bg-[#85132c] transition duration-300 w-full md:w-auto"
         >
           Explore Courses
         </button>
         <button
-          className="bg-[#85132c]  text-sm text-white px-1 py-3 rounded-md shadow-md font-bold hover:bg-[#85132c] transition duration-300 w-full md:w-auto"
+          className="bg-[#85132c]  text-sm text-white px-4 py-3 rounded-md shadow-md font-bold hover:bg-[#85132c] transition duration-300 w-full md:w-auto"
         >
           Enroll Now
         </button>
